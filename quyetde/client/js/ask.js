45cfbdf1c025eff4e3cd97b9393ae64f472890ea
huynhtuanhuy <huynhtuanhuy1996@gmail.com>
2018-09-14 20:38:13 +0700

    a

--- quyetde/client/js/ask.js
+++ quyetde/client/js/ask.js
@@ -2,4 +2,15 @@ $(document).ready(function() {
   $('#questionInput').on("input", function() {
     $('#count').text($('#questionInput').attr('maxlength') - $('#questionInput').val().length);
   });
+
+  $('#askForm').on('submit', function(e) {
+    e.preventDefault();
+  });
+empty()
+html('')
+  `${dsada}
+  saddas<a href="http://${asdas}">
+  sdasadsa
+  adsdsadsa${dsada}
+  sad`
 });
